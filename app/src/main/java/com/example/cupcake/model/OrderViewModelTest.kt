51dbce503f0aff17763ef7891241c7b4b37641d4
Kenneth Mathari <mathari580@gmail.com>
2022-06-24 21:25:10 +0300

    added LiveData tests

--- app/src/main/java/com/example/cupcake/model/OrderViewModelTest.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModelTest.kt
@@ -1,6 +1,8 @@
 package com.example.cupcake.model
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -8,12 +10,23 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class OrderViewModelTest {
 
+    val orderViewModel = OrderViewModel()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @Test
-    fun cupcakes() {
-        val orderViewModel = OrderViewModel()
+    fun quantity_twelve_cupcakes() {
         orderViewModel.quantity.observeForever {}
         orderViewModel.setQuantity(12)
         assertEquals(12, orderViewModel.quantity.value)
+    }
+
+    @Test
+    fun price_twelve_cupcakes(){
+        orderViewModel.price.observeForever {}
+        orderViewModel.setQuantity(12)
+        assertEquals("$27.00", orderViewModel.price.value)
     }
 
 }
